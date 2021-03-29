adf9823ad40100de031f00293df3550a33786bb0
beckkramer <kramer.beck@gmail.com>
2021-03-29 08:31:42 -0500

    Adds titleCase

--- src/letterpress.spec.ts
+++ src/letterpress.spec.ts
@@ -1,8 +1,9 @@
 
-import { tidyLines } from './letterpress'
+import { tidyLines, titleCase } from './letterpress'
 
 
 describe('tidyLines', () => {
+
   test('Add a &nbsp; between the last and second-to-last words in the string', () => {
     const originalString = 'This string contains a full sentence.'
     const formattedString = tidyLines(originalString)
@@ -15,5 +16,22 @@ describe('tidyLines', () => {
     const formattedString = tidyLines(originalString)
   
     expect(formattedString.includes('&nbsp;')).toEqual(false)
+  })
+})
+
+describe('titleCase', () => {
+
+  test('Returns a string in title case', () => {
+
+    const byStyleguideString = 'this string checks for how The styleguides capitalize to see how they do!'
+
+    const chicagoStyleString = titleCase(byStyleguideString)
+    expect(chicagoStyleString).toEqual('This String Checks for How the Styleguides Capitalize to See How They Do!')
+
+    const apStyleString = titleCase(byStyleguideString, 'ap')
+    expect(apStyleString).toEqual('This String Checks For How The Styleguides Capitalize to See How They Do!')
+
+    const nytStyleString = titleCase(byStyleguideString, 'nyt')
+    expect(nytStyleString).toEqual('This String Checks for How the Styleguides Capitalize to See How They Do!')
   })
 })
--- src/letterpress.ts
+++ src/letterpress.ts
@@ -1,14 +1,59 @@
-export const tidyLines = (string: string) => {
-  if (typeof string !== 'string') throw new TypeError('Please provide a string.')
+import { articles, conjunctions, prepositions } from './wordTypes'
 
-  const stringArray = string.split(' ')
+export const tidyLines = (originalString: string) => {
+  if (typeof originalString !== 'string') throw new TypeError('Please provide a string.')
+
+  const stringArray = originalString.split(' ')
 
   if (stringArray.length === 1) {
-    return string
+    return originalString
   }
 
   const lastWord = stringArray.pop()
   const formattedString = stringArray.join(' ') + '&nbsp;' + lastWord
 
   return formattedString
+}
+
+type Styleguide = 'chicago' | 'ap' | 'nyt'
+
+export const titleCase = (originalString: string, styleGuide:Styleguide = 'chicago') => {
+  if (typeof originalString !== 'string') throw new TypeError('Please provide a string.')
+
+  const capitalizeFirstLetter = (textEntry: string) => {
+    const stringArray = textEntry.split('')
+    const firstLetter = stringArray.shift()?.toUpperCase()
+
+    return firstLetter + stringArray.join('').toLowerCase()
+  }
+
+  const stringArray = originalString.split(' ')
+ 
+  const formattedWords = stringArray.map((entry, index) => {
+
+    // Chicago, NTY, and AP styles capitalize the first and last words
+    if (index === 0 || index === stringArray.length - 1) {
+      return capitalizeFirstLetter(entry)
+    }
+
+    if (styleGuide === 'ap' && entry.length >= 3) {
+      return capitalizeFirstLetter(entry)
+    }
+
+    if (styleGuide === 'chicago' && entry.length >= 4) {
+      return capitalizeFirstLetter(entry)
+    }
+
+    if (
+      !!articles.includes(entry.toLowerCase())
+      || !!conjunctions.includes(entry.toLowerCase())
+      || !!prepositions.includes(entry.toLowerCase())
+    ) {
+      return entry.toLowerCase()
+    }
+
+    return capitalizeFirstLetter(entry)
+  })
+
+  return formattedWords.join(' ')
 }
